994d4a2bad005f2fa05c3d1ebb35fe71863c1a6c
“Elie <elie.maj@hotmail.com>
2022-05-28 21:28:40 +0300

    editing the add restaurant functionality

--- add.php
+++ add.php
@@ -1,7 +1,11 @@
 <?php
 
 include("connection.php");
-
+$name=$_GET["name"];
+$description=$_GET["description"];
+$icon=$_GET["icon"];
+$phone=$_GET["phone"];
+$location=$_GET["location"];
 
 $query = $mysqli->prepare("INSERT INTO restaurants (name, description, icon, phone, location) VALUES (?, ?, ?, ?, ?)");
 $query->bind_param("sssss", $name, $description ,$icon ,$phone ,$location);
