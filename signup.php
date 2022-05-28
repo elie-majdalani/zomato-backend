9a883466057e1f6e266cba223ad7d033149d98c5
“Elie <elie.maj@hotmail.com>
2022-05-29 00:03:43 +0300

    adding signup email availability check

--- signup.php
+++ signup.php
@@ -4,7 +4,22 @@ include("connection.php");
 $email = $_GET["email"];
 $password = hash("sha256", $_GET["password"]);
 
-$query = $mysqli->prepare("INSERT INTO users (password, email) VALUES (?, ?)");
-$query->bind_param("ss", $password, $email);
+$query = $mysqli->prepare("Select id from users where email = ? AND password = ?");
+$query->bind_param("ss", $email, $password);
 $query->execute();
+$query->store_result();
+$num_rows = $query->num_rows;
+$query->bind_result($id,$role);
+$query->fetch();
+$response = [];
+
+if($num_rows == 1){
+    $response["response"] = "used";
+}else{
+    $query = $mysqli->prepare("INSERT INTO users (password, email) VALUES (?, ?)");
+    $query->bind_param("ss", $password, $email);
+    $query->execute();
+    $response["response"] = "Logged in";
+}
+
 ?>
