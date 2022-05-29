2801c16398eb51c59d069a1d71de9941ad125a4d
“Elie <elie.maj@hotmail.com>
2022-05-29 16:50:09 +0300

    testing the add restaurant functionality

--- add.php
+++ add.php
@@ -12,8 +12,7 @@ $query = $mysqli->prepare("INSERT INTO restaurants (name, description, icon, pho
 $query->bind_param("sssss", $name, $description ,$icon ,$phone ,$location);
 $query->execute();
 
-// $query = $mysqli->prepare("INSERT INTO restaurant_has_cuisines restaurant_id, cuisine_id INNER JOIN cuisines c on cuisine_id = c.id where $id = ?");
-
-// select r.id, c.id from restaurants r INNER JOIN cuisines c on r.name = "KFC" and c.cuisine= "Italian";
+// insert into restaurant_has_cuisines select r.id, c.id from restaurants r INNER JOIN cuisines c on r.name = "KFC" and c.cuisine= "Italian";
+// $query = $mysqli->prepare("INSERT INTO restaurant_has_cuisines restaurant_id, cuisine_id INNER JOIN cuisines c on cuisine_id = c.id ");
 
 ?>
--- signup.php
+++ signup.php
@@ -1,25 +1,25 @@
 <?php
 include("connection.php");
 
-$email = $_GET["email"];
-$password = hash("sha256", $_GET["password"]);
+$username = $_POST['username'];
+$email = $_POST["email"];
+$password = hash("sha256", $_POST["password"]);
 
-$query = $mysqli->prepare("Select id from users where email = ? AND password = ?");
-$query->bind_param("ss", $email, $password);
+$query = $mysqli->prepare("Select id from users where username = ? OR email = ?");
+$query->bind_param("ss",$username, $email);
 $query->execute();
 $query->store_result();
 $num_rows = $query->num_rows;
-$query->bind_result($id,$role);
 $query->fetch();
 $response = [];
 
 if($num_rows == 1){
     $response["response"] = "used";
 }else{
-    $query = $mysqli->prepare("INSERT INTO users (password, email) VALUES (?, ?)");
-    $query->bind_param("ss", $password, $email);
+    $query = $mysqli->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
+    $query->bind_param("sss",$username, $password, $email);
     $query->execute();
-    $response["response"] = "Logged in";
+    $response["response"] = "ok";
 }
 
 ?>
