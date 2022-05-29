06e6ed0d381437adbcc6e66cc7e38cfe293cea14
“Elie <elie.maj@hotmail.com>
2022-05-29 16:58:29 +0300

    finalising the add restaurant functionality

--- add.php
+++ add.php
@@ -12,7 +12,11 @@ $query = $mysqli->prepare("INSERT INTO restaurants (name, description, icon, pho
 $query->bind_param("sssss", $name, $description ,$icon ,$phone ,$location);
 $query->execute();
 
-// insert into restaurant_has_cuisines select r.id, c.id from restaurants r INNER JOIN cuisines c on r.name = "KFC" and c.cuisine= "Italian";
-// $query = $mysqli->prepare("INSERT INTO restaurant_has_cuisines restaurant_id, cuisine_id INNER JOIN cuisines c on cuisine_id = c.id ");
+for (i=0; i<count($cuisines); i++) {
+    $query = $mysqli->prepare("INSERT INTO cuisines (restaurant_id, cuisine_id) VALUES ((SELECT id FROM restaurants WHERE name = ?), (SELECT id FROM cuisines WHERE cuisine_name = ?))");
+    $query->bind_param("ss", $name, $cuisines[i]);
+    $query->execute();
+}
+//$query = $mysqli->prepare("insert into restaurant_has_cuisines (restaurant_id, cuisine_id) select r.id, c.id from restaurants r INNER JOIN cuisines c on r.name = "KFC" and c.cuisine= "Italian";");
 
 ?>
