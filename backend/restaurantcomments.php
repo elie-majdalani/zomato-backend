41673aae3fdca761add3a0ab99856c780db90de3
“Elie <elie.maj@hotmail.com>
2022-05-31 06:34:31 +0300

    fixing restaurant comments reurned params

--- backend/restaurantcomments.php
+++ backend/restaurantcomments.php
@@ -1,10 +1,10 @@
 <?php
 include("connection.php");
 
-$id = $_GET["id"];
+$id = $_GET["resto_id"];
 
 
-$query = $mysqli->prepare("SELECT * FROM comments INNER JOIN users ON users.id = comments.users_id WHERE comments.restaurants_id = ? AND comments.accepted=1");
+$query = $mysqli->prepare("SELECT comments.review, comments.rating, comments.date, users.username  FROM comments INNER JOIN users ON users.id = comments.users_id WHERE comments.restaurants_id = ? AND comments.accepted=1");
 
 //SELECT r.name , r.id , cu.cuisine,u.email, c.review, c.date from restaurants as r INNER JOIN comments c on r.id=c.restaurants_id INNER join users u on u.id=c.users_id INNER JOIN restaurant_has_cuisines rhc on r.id= rhc.restaurant_id INNER JOIN cuisines cu on rhc.cuisine_id = cu.id where r.id=2 GROUP by cu.cuisine;
 
