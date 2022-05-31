fb0309604a76a79b0d556d89dbc98e632492f36f
“Elie <elie.maj@hotmail.com>
2022-05-31 04:47:39 +0300

    adding the admin statistics page link

--- draft/scripts/admin.js
+++ draft/scripts/admin.js
@@ -1,12 +1,12 @@
-const users =document.getElementById("users").value;
-const restaurants = document.getElementById("restaurants").value;
-const comments = document.getElementById("omments").value; {
+const users =document.getElementById("users");
+const restaurants = document.getElementById("restaurants");
+const comments = document.getElementById("comments"); 
     
 
         let data = {
-            users: users,
-            restaurants : restaurants,
-            reviews :reviews,
+            users: users.value,
+            restaurants : restaurants.value,
+            comments :comments.value,
         }
 
         let url = 'http://localhost/zomato/backend/statistics.php';
@@ -18,17 +18,16 @@ const comments = document.getElementById("omments").value; {
         .then(function (response) {
             console.log(response)
             let result = response.data;
-            let usersphp = result[0];
-            let reviewsphp = result[1];
-            let restaurantsphp = result[2];
-            users.innerHtml=usersphp[1];
-            restaurants.innerHtml=restaurantsphp[1];
-            comments.innerHtml=commentsphp[1];
+            let usersphp = result[0].users;
+            let commentsphp = result[0].comments;
+            let restaurantsphp = result[0].restaurants;
+            users.innerHTML=usersphp;
+            restaurants.innerHTML=restaurantsphp;
+            comments.innerHTML=commentsphp;
 
             
-            window.location.href = "http://localhost/zomato/draft/profile.html";
 
         });
         
             
-  }
+  
